068e3bbfd26a5f82c341ece1d814bbe6a02d7575
Yana Maximkina <djanglboop@gmail.com>
2020-02-11 12:16:55 +0900

    Setting the navbar

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'materialize-sass', '~> 1.0.0'
 # Icons
 gem 'material_icons'
+# JQuery
+gem 'jquery-rails'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,26 @@
-// This is a manifest file that'll be compiled into including all the files listed below.
-// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// be included in the compiled file accessible from http://example.com/assets/application.js
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-//= require jquery
-//= require jquery_ujs
-//= require materialize
-//= require turbolinks
-//= require_tree .
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
 
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require('jquery')
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+import '../stylesheets/application'
+import 'materialize-css/dist/js/materialize'
+
+$(document).on('turbolinks:load', function() {
+        $(".dropdown-trigger").dropdown();
+        $('.sidenav').sidenav();
+    })
